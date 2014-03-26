4997bd47e2ea3bd4b4ba0213580246d51638502a
Gareth McDonald <mcdonagg@gmail.com>
2014-03-26 07:13:33 -0400

    Played with Seasons view

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,7 +2,9 @@ class SeasonsController < ApplicationController
   def show
       @season_number = params[:season_num]
       @season_show = Show.find_by_id(params[:season_show])
+
         if @season_show
+            @episodes = @season_show.episodes.all
             render action: :show
         else
             render file: 'public/404', status: 404, formats: [:html]
